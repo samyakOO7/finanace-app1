184accb8789e1ef0d637b56d855295b3e09cb1cd
shravanilongview <shravani.longview@gmail.com>
2020-08-13 16:54:03 +0530

    Buttons added

---
+++ lib/HomePage/homepage.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+import '../menu_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Color(0xff373D3F),
+        ),
+        backgroundColor: Color(0xff63E2E0),
+        centerTitle: true,
+        title: Text(
+          'HOME',
+          style: TextStyle(
+            color: Color(0xff373D3F),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: menuPage(),
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints viewportConstraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: viewportConstraints.maxHeight,
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(18.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Hi User',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff373D3F),
+                          ),
+                        ),
+                        IconButton(
+                          iconSize: 40,
+                          onPressed: () {},
+                          icon: Icon(Icons.notifications_none),
+                          color: Color(0xff373D3F),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 5.0, // soften the shadow
+                                spreadRadius: 0, //extend the shadow
+                              )
+                            ]),
+                        height: height * 0.25,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Column(
+                                    children: <Widget>[
+                                      Text('Invested'),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text('43,74,652.34'),
+                                    ],
+                                  ),
+                                  Column(
+                                    children: <Widget>[
+                                      Text('Current'),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text('51,80,487.55'),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Divider(
+                                color: Colors.grey,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text('P&L'),
+                                    ],
+                                  ),
+                                  Column(
+                                    children: <Widget>[
+                                      Text(
+                                        '+8,05,835.20',
+                                        style: TextStyle(color: Colors.green),
+                                      ),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        '+18.42%',
+                                        style: TextStyle(color: Colors.green),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
