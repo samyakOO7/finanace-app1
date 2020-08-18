b12557789fe65b58f2853d150d4a08444347305c
riyalongview <68843385+riyalongview@users.noreply.github.com>
2020-08-18 19:26:55 +0530

    Create touchID.dart

---
+++ lib/touchID.dart
@@ -0,0 +1,53 @@
+
+import 'package:finance_app/HomePage.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:local_auth/local_auth.dart';
+
+class TouchScreen extends StatelessWidget {
+  final LocalAuthentication localAuth = LocalAuthentication();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () async {
+          bool weCanCheckBiometrics = await localAuth.canCheckBiometrics;
+
+          if (weCanCheckBiometrics) {
+            bool authenticated = await localAuth.authenticateWithBiometrics(
+              localizedReason: "Authenticate to see your bank statement.",
+            );
+
+            if (authenticated) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomeScreen(),
+                ),
+              );
+            }
+          }
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(
+              Icons.fingerprint,
+              size: 124.0,
+            ),
+            Text(
+              "Touch to Login",
+              style: GoogleFonts.passionOne(
+                fontSize: 64.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
