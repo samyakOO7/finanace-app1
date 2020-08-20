9a2a0db60529758499909ca4ca9e477b31810829
riyalongview <68843385+riyalongview@users.noreply.github.com>
2020-08-20 14:09:01 +0530

    Update SignIn_page.dart

--- lib/SignUP_PageWith_Chnages/SignIn_page.dart
+++ lib/SignUP_PageWith_Chnages/SignIn_page.dart
@@ -1,8 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:finance_app/HomePage.dart';
+import 'package:finance_app/forget_password.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:string_validator/string_validator.dart' as st_validator;
 import 'Widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:finance_app/authentication.dart';
+import 'SignUp_page.dart';
 
 import 'SignUp_page.dart';
 
@@ -12,9 +22,128 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  //final _formKey2 = GlobalKey<FormState>();
+  
+  bool _isHidden = true;
+
+  void _toggleVisibility(){
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  SharedPreferences preferences;
+
+
+  bool isLoggedIn = false;
+  bool isLoading = false;
+  FirebaseUser currentUser;
+
+
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  var _isProcessing;
+
+  @override
+  void initState() {
+    super.initState();
+    _isProcessing = false;
+
+    isSignedIn();
+  }
+
+  void isSignedIn() async {
+    this.setState(() {
+      isLoggedIn = true;
+    });
+
+    preferences = await SharedPreferences.getInstance();
+
+    isLoggedIn = await googleSignIn.isSignedIn();
+
+    if (isLoggedIn) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) =>
+          HomeScreen(currentUserId: preferences.getString("id"))));
+    }
+    this.setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   final _formKey2 = GlobalKey<FormState>();
+
+  Map<String, String> _authData= {
+    'email' : '',
+    'password' : ''
+
+  };
+
+  void validateAndSave() {
+    final FormState form = _formKey2.currentState;
+    if (form.validate()) {
+      print('Form is valid');
+    } else {
+      print('Form is invalid');
+    }
+  }
+
+  Future<void> _submit() async
+  {
+    if(!_formKey2.currentState.validate())
+    {
+      return;
+    }
+    _formKey2.currentState.save();
+
+    try{
+      await  Provider.of<Authentication>(context, listen: false).
+      logIn(_authData['email'],
+          _authData['password']);
+
+      Navigator.pushReplacement(context, MaterialPageRoute(
+          builder: (BuildContext context) => HomeScreen()
+      ));
+    }
+    catch(error)
+    {
+      var errorMessage = 'Authentication Failed. Please try again';
+      _showErrorDailog(errorMessage);
+    }
+
+
+  }
+  void _showErrorDailog(String msg)
+  {
+    showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text('An Error Occurred'),
+          content: Text(msg),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Okay'),
+              onPressed: () {
+                Navigator.of(ctx).pop();
+              },
+            )
+          ],
+        )
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
+  
     var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
@@ -45,6 +174,7 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                     ),
                     Form(
+                      key: _formKey2,
                       child: Column(
                         children: [
                           TextFormField(
@@ -53,26 +183,47 @@ class _LoginPageState extends State<LoginPage> {
                               if (value1.isEmpty) {
                                 return 'Please enter an email address';
                               }
-                              if (st_validator.isEmail(value1)) {
-                                return 'Enter a valid email address';
-                              }
-                              if (value1.split('@').length != 2) {
-                                return 'Enter a valid email address';
-                              }
+//                              if (st_validator.isEmail(value1)) {
+//                                return 'Enter a valid email address';
+//                              }
+//                              if (value1.split('@').length != 2) {
+//                                return 'Enter a valid email address';
+//                              }
                               return null;
+                            },
+                            onSaved: (value)
+                            {
+                              _authData['email']=value;
                             },
                           ),
                           SizedBox(
                             height: 20,
                           ),
                           TextFormField(
-                            obscureText: true,
-                            decoration: textfield("Password"),
+                            obscureText: _isHidden,
+
+
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(0.5),
+                              ),
+                              suffixIcon: IconButton(
+                                onPressed: _toggleVisibility,
+                                icon: Icon(Icons.visibility_off),
+                              )
+
+                            ),
+
                             validator: (value1) {
                               if (value1.isEmpty) {
                                 return 'Please enter a password';
                               }
                               return null;
+                            },
+                            onSaved: (value)
+                            {
+                              _authData['password']=value;
                             },
                           ),
                         ],
@@ -85,7 +236,12 @@ class _LoginPageState extends State<LoginPage> {
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: <Widget>[
                         GestureDetector(
-                          onTap: () {},
+                          onTap: () {
+                            Navigator.pushReplacement(context, MaterialPageRoute(
+                                builder: (BuildContext context) => ForgetPassPage()
+                            ));
+
+                          },
                           child: Text('Forgot Password?',
                               style: TextStyle(
                                   fontWeight: FontWeight.w500,
@@ -99,9 +255,8 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     RaisedButton(
                       onPressed: () {
-                        setState(() {
-                          if (_formKey2.currentState.validate()) ;
-                        });
+                        _submit();
+                        validateAndSave();
                       },
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
@@ -179,7 +334,7 @@ class _LoginPageState extends State<LoginPage> {
                       width: width * 0.7,
                       child: RaisedButton(
                         color: Colors.white,
-                        onPressed: () {},
+                        onPressed: controlSignIn,
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
@@ -204,5 +359,82 @@ class _LoginPageState extends State<LoginPage> {
         );
       }),
     );
+  }
+  Future<Null> controlSignIn() async {
+    preferences = await SharedPreferences.getInstance();
+    this.setState(() {
+      isLoading = true;
+    });
+
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuthentication = await googleUser
+        .authentication;
+    final AuthCredential credential = GoogleAuthProvider
+        .getCredential(idToken: googleAuthentication.idToken,
+        accessToken: googleAuthentication.accessToken);
+
+    FirebaseUser firebaseUser = (await firebaseAuth.signInWithCredential(
+        credential)).user;
+
+// Sign in success
+
+    if (firebaseUser != null) {
+      // if already signed up
+
+      final QuerySnapshot resultQuery = await Firestore.instance
+          .collection("users")
+          .where("id", isEqualTo: firebaseUser.uid)
+          .getDocuments();
+      final List<DocumentSnapshot> documentSnapshots = resultQuery.documents;
+
+      // if user is new
+
+      if (documentSnapshots.length == 0) {
+        Firestore.instance.collection("users")
+            .document(firebaseUser.uid)
+            .setData({
+
+          "username": firebaseUser.displayName,
+          "photoUrl": firebaseUser.photoUrl,
+          "createdAt": DateTime
+              .now()
+              .millisecondsSinceEpoch
+              .toString(),
+        });
+
+        // write data locally
+
+        currentUser = firebaseUser;
+        await preferences.setString("id", currentUser.uid);
+        await preferences.setString("username", currentUser.displayName);
+        await preferences.setString("photoUrl", currentUser.photoUrl);
+      }
+      else {
+        //retrieve data from firebase
+        currentUser = firebaseUser;
+        await preferences.setString("id", documentSnapshots[0]["id"]);
+        await preferences.setString(
+            "username", documentSnapshots[0]["username"]);
+        await preferences.setString(
+            "photoUrl", documentSnapshots[0]["photoUrl"]);
+      }
+
+      Fluttertoast.showToast(msg: "Sign In Success !!");
+      this.setState(() {
+        isLoading = false;
+      });
+
+      Navigator.push(context, MaterialPageRoute(
+          builder: (context) => HomeScreen(currentUserId: firebaseUser.uid)));
+    }
+
+// sign in failed
+
+    else {
+      Fluttertoast.showToast(msg: "Please try again !!");
+      this.setState(() {
+        isLoading = false;
+      });
+    }
   }
 }
